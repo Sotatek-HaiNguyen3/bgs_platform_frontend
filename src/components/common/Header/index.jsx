88343ce33ed425c81692084424678d8310eeed8b
sotatek-tutran2 <tu.tran2@sotatek.com>
2022-03-14 15:50:18 +0700

    move header content to container

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -9,35 +9,39 @@ const Header = () => {
 
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>
-        <h1>BGS</h1>
-      </div>
-      <nav className={styles.nav}>
-        <ul className={styles.links}>
-          <li>
-            <Link to="/dashboard">My dashboard</Link>
-          </li>
-          <li>
-            <Link to="/video-store">Video store</Link>
-          </li>
-          <li>
-            <Link to="/bounty-board">Bounty board</Link>
-          </li>
-          <li>
-            <Link to="/job-board">Job board</Link>
-          </li>
-          <li>
-            <Link to="/faq">FAQ</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className={styles.actions}>
-        <Button title="Sign up" onClick={() => navigate("/register")} />
-        <Button
-          title="Login"
-          onClick={() => navigate("/login")}
-          icon={<FaUser />}
-        />
+      <div className="container">
+        <div className={styles.headerInner}>
+          <div className={styles.logoContainer}>
+            <h1>BGS</h1>
+          </div>
+          <nav className={styles.nav}>
+            <ul className={styles.links}>
+              <li>
+                <Link to="/dashboard">My dashboard</Link>
+              </li>
+              <li>
+                <Link to="/video-store">Video store</Link>
+              </li>
+              <li>
+                <Link to="/bounty-board">Bounty board</Link>
+              </li>
+              <li>
+                <Link to="/job-board">Job board</Link>
+              </li>
+              <li>
+                <Link to="/faq">FAQ</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.actions}>
+            <Button title="Sign up" onClick={() => navigate("/register")} />
+            <Button
+              title="Login"
+              onClick={() => navigate("/login")}
+              icon={<FaUser />}
+            />
+          </div>
+        </div>
       </div>
     </header>
   );
