393d5c7ff5da8f8c27c0fb99fd08434a12e0b348
sotatek-tutran2 <tu.tran2@sotatek.com>
2022-03-15 11:11:47 +0700

    feature/ng-205: add toast notification

---
+++ src/components/common/ToastContainer/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+
+const CustomToastContainer = () => {
+  return (
+    <ToastContainer
+      position="top-center"
+      autoClose={4000}
+      closeOnClick
+      newestOnTop
+      pauseOnHover
+      className="toast-custom"
+    />
+  );
+};
+
+export default CustomToastContainer;
